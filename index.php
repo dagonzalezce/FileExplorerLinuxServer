c76023aed87f952467c0440cdb215a8b8827f92b
Mayra Tejada <44760892+MayraTejada@users.noreply.github.com>
2020-02-18 13:48:52 -0500

    funcionar el botón mover

--- index.php
+++ index.php
@@ -193,11 +193,11 @@
       </div>
       <div class="modal-body">
       	<p> Ingrese la ruta del directorio al que desea mover este archivo/directorio </p>
-        <input type="text" class="form-control" >
+        <input type="text" class="form-control" id="newDirectionInput" >
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-light btn-outline-dark" data-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-primary btn-outline-light" style="background-color: #E8520C;">Aceptar</button>
+        <button onclick="moveFile();" type="button" class="btn btn-primary btn-outline-light" style="background-color: #E8520C;">Aceptar</button>
       </div>
     </div>
   </div>
